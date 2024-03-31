653945c519b35a2bddbec53693d474d23e7fa4ea
Mattia Martinoni <martinonimattia@hotmail.com>
2024-03-31 20:30:05 +0200

    Creato signin api routes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+// import { restart } from 'nodemon';
 
 export const signup = async (req, res, next) => {
     
@@ -16,7 +18,12 @@ export const signup = async (req, res, next) => {
      * non sono true o siano una stringa vuota.
      * Come compilare campi nel form.
      * */
-    if(!email || !password || email === '' || password === '') {
+    if(!username ||
+        !email ||
+        !password ||
+        username === '' ||
+        email === '' ||
+        password === '') {
         next(errorHandler(400, 'Tutti i campi sono obbligatori'))
     }
 
@@ -36,5 +43,40 @@ export const signup = async (req, res, next) => {
         
     } catch(err) {
         next(err)
+    }
+}
+
+export const signin = async (req, res, next) => {
+
+    const { email, password } = req.body
+
+    if(!email || !password || email === '' || password === '') {
+        next(errorHandler(400, 'Tutti i campi sono obbligatori'))
+    }
+
+    try {
+
+        const validUser = await User.findOne({ email })
+
+        if(!validUser) {
+            return next(errorHandler(404, 'User not found'))
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+
+        if(!validPassword) {
+            return next(errorHandler(400, 'Invalid password'))
+        }
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+
+        // Ritorna validUser senza password
+        const { password: pass, ...rest } = validUser._doc
+
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true}).json(rest)
+
+    } catch(error) {
+        next(error)
     }
 }
