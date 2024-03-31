0261754946a7c734bd4be30b452e713207b130ff
Mattia Martinoni <martinonimattia@hotmail.com>
2024-03-31 12:16:09 +0200

    Stabilito connessione con la banca dati

---
+++ api/index.js
@@ -0,0 +1,18 @@
+// require('dotenv').config()
+import dotenv from 'dotenv'
+import express from 'express';
+import mongoose from 'mongoose';
+
+dotenv.config()
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log('Connected To MongoDb')
+})
+.catch(err => console.error(err))
+
+app.listen(3000, () => {
+    console.log(`Server is running on port 3000`)
+})
