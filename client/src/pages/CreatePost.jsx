7a6068f129fcfb899e4928ab54ee8b78e5394242
Mattia Martinoni <martinonimattia@hotmail.com>
2024-04-03 21:54:02 +0200

    Aggiunto posts section alla dashboard

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,7 +6,7 @@ import { getStorage, uploadBytesResumable, ref, getDownloadURL } from 'firebase/
 import { app } from '../firebase'
 import { CircularProgressbar } from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css'
-
+import { useNavigate } from 'react-router-dom'
 
 
 export default function CreatePost() {
@@ -15,6 +15,10 @@ export default function CreatePost() {
   const [imageUploadProgress, setImageUploadProgress] = useState(null)
   const [imageUploadError, setImageUploadError] = useState(null)
   const [formData, setFormData] = useState({})
+  const [publishError, setPublishError] = useState(null)
+
+  // console.log(formData)
+  const navigate = useNavigate()
 
   const handleUploadImage = async() => {
     
@@ -38,6 +42,7 @@ export default function CreatePost() {
         (error) => {
           setImageUploadError('Image upload failed')
           setImageUploadProgress(null)
+          console.log(error)
         },
         () => {
           getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
@@ -55,15 +60,41 @@ export default function CreatePost() {
     }
   }
 
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+    try {
+
+      const res = await fetch('/api/post/create', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData)
+      })
+
+      const data = await res.json()
+
+      if(!res.ok) {
+        setPublishError(data.message)
+        return
+      }
+      if(res.ok) {
+        setPublishError(null)
+        navigate(`/post/${data.slug}`)
+      }
+
+    } catch(error) {
+      setPublishError('Something went wrong')
+    }
+  }
+
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
-      <h1 className='text-center text-3xl my-7 font-semibold'>
-        Create a Post
-      </h1>
-      <form className='flex flex-col gap-4'>
+      <h1 className='text-center text-3xl my-7 font-semibold'>Create a Post</h1>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
         <div className='flex flex-col gap-4 sm:flex-row justify-between'>
-          <TextInput type='text' placeholder='Title' required id='title' className='flex-1'/>
-          <Select>
+          <TextInput type='text' placeholder='Title' required id='title' className='flex-1'
+          onChange={(e) => setFormData({ ...formData, title: e.target.value })}/>
+
+          <Select onChange={(e) => setFormData({ ...formData, category: e.target.value })}>
             <option value='uncategorized'>Select a category</option>
             <option value='travel'>viaggi</option>
             <option value='drink-and-food'>Cibo e bevande</option>
@@ -71,6 +102,7 @@ export default function CreatePost() {
             <option value='economy'>Economia</option>
             <option value='economy'>Attività</option>
           </Select>
+
         </div>
         <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
           <FileInput type='file' accept='image/*' onChange={(e) => setFile(e.target.files[0]) }/>
@@ -94,10 +126,13 @@ export default function CreatePost() {
         { formData.image && (
           <img src={formData.image} alt='upload' className='w-full h-72 object-cover' />
         )}
-        <ReactQuill theme='snow' placeholder='Write something..' className='h-72 mb-12' required />
+        <ReactQuill theme='snow' placeholder='Write something..' className='h-72 mb-12' required 
+        onChange={(value) => setFormData({ ...formData , content: value })}/>
+
         <Button type='submit' gradientDuoTone='purpleToBlue'>
           Publish
         </Button>
+        { publishError && <Alert className='mt-5' color='failure'>{ publishError }</Alert> }
       </form>
     </div>
   )
