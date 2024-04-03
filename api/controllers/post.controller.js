5e9d5608e3fa7d247d70686368304eca4d0bbe57
Mattia Martinoni <martinonimattia@hotmail.com>
2024-04-04 01:36:23 +0200

    Mostra i post degli utenti nella dashboard

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -36,10 +36,10 @@ export const getposts = async (req, res, next) => {
         const limit = parseInt(req.query.limit) || 9
         const sortDirection = req.query.order === 'asc' ? 1 : -1
         const posts = await Post.find({
-            ...(req.query.userId && { userId: req.user.userId }),
-            ...(req.query.category && { category: req.user.category }),
-            ...(req.query.slug && { slug: req.user.slug }),
-            ...(req.query.postId && { _id: req.user.postId }),
+            ...(req.query.userId && { userId: req.query.userId }),
+            ...(req.query.category && { category: req.query.category }),
+            ...(req.query.slug && { slug: req.query.slug }),
+            ...(req.query.postId && { _id: req.query.postId }),
             ...(req.query.searchTerm && {
                 $or: [
                     { title: { $regex: req.query.searchTerm, $options: 'i' } },
