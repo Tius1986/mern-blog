1165f6555788ae09e002b41ce270d2155fc1fa07
Mattia Martinoni <martinonimattia@hotmail.com>
2024-04-05 15:06:33 +0200

    Mostra gli utenti nella admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import { Sidebar } from 'flowbite-react'
-import { HiUser, HiArrowSmRight, HiDocumentText } from 'react-icons/hi'
+import { HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup } from 'react-icons/hi'
 import { useEffect, useState } from 'react'
 import { useLocation, Link } from 'react-router-dom'
 import { signoutSuccess } from '../redux/user/userSlice'
@@ -52,10 +52,16 @@ export default function DashSidebar() {
             </Sidebar.Item>
           </Link>
           {currentUser.isAdmin && (
-
             <Link to='/dashboard?tab=posts'>
               <Sidebar.Item active={tab === 'posts' } icon={ HiDocumentText } as='div'>
                 Posts
+              </Sidebar.Item>
+            </Link>
+          )}
+          {currentUser.isAdmin && (
+            <Link to='/dashboard?tab=users'>
+              <Sidebar.Item active={tab === 'users' } icon={ HiOutlineUserGroup } as='div'>
+                Users
               </Sidebar.Item>
             </Link>
           )}
